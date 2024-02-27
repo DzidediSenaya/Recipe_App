464c8f701c3ea622902e28366815ec307d51df30
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-02-27 12:16:56 +0000

    added search on enter feature

--- src/pages/recipes/index.jsx
+++ src/pages/recipes/index.jsx
@@ -4,40 +4,49 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 
+
 export default function Recipes() {
-    //prepare url
-    const url = new URL('https://api.spoonacular.com/recipes/complexSearch');
-    url.searchParams.append('apiKey', '2a8aa44ca9004671a4948178c1ebc72a')
     const [recipes, setRecipes] = useState([]);
+    const [searchItem, setSearchItem] = useState("");
 
     const searchRecipes = () => {
-        //fetch recipes
-        fetch(url)
-            .then((response) => response.json())
-            .then((data) => {//update the recipes state
-                setRecipes(data.results);
-                //console.log(data);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
+        //prepare url
+        const url = new URL('https://api.spoonacular.com/recipes/complexSearch');
+        url.searchParams.append('apiKey', process.env.REACT_APP_SPOONACULAR_API_KEY);
+        url.searchParams.append('query', searchItem);//Add the query parameter
 
+            //fetch recipes
+            fetch(url)
+                .then((response) => response.json())
+                .then((data) => {//update the recipes state
+                    setRecipes(data.results);
+                    //console.log(data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+
+        }
+
+        useEffect(searchRecipes, []);
+
+        return (
+            <Container sx={{ my: '2rem' }}>
+                <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    label="Enter a keyword to search recipes and hit Enter"
+                    variant="outlined"
+                    value={searchItem}
+                    onChange={(event) => setSearchItem(event.
+                        target.value)}
+                    onKeyDown={event => event.key == 'Enter' &&
+                        searchRecipes()}
+                />
+                <Grid sx={{ mt: '1rem' }} container spacing={3}>
+                    {recipes.map((recipe) => <RecipeItem key={recipe.id} title={recipe.title} image={recipe.image} />
+                    )}
+                </Grid>
+            </Container>
+        );
     }
-
-    useEffect(searchRecipes, []);
-
-    return (
-        <Container sx={{ my: '2rem' }}>
-            <TextField
-                fullWidth
-                id="outlined-basic"
-                label="Enter a keyword to search recipes and hit Enter"
-                variant="outlined"
-            />
-            <Grid sx={{ mt: '1rem' }} container spacing={3}>
-                {recipes.map((recipe) => <RecipeItem  key={recipe.id} title={recipe.title} image={recipe.image}/>
-                )}
-            </Grid>
-        </Container>
-    );
-}
