a56a1a40a27c0b2b7aa2f108cfc8ec679c2d8182
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-03-21 17:00:17 +0000

    incorporated new api into recipe app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Navbar from "./components/navbar";
 import Recipes from "./pages/recipes";
 import Recipe from "./pages/recipe";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AddRecipe from "./pages/add-recipe";
 
 const router = createBrowserRouter([
   {path: "/", element: <Recipes />},
   {path: "/recipes", element: <Recipes />},
-  {path: "/recipes/:id", element: <Recipe />}
+  {path: "/recipes/:id", element: <Recipe />},
+  {path: "/add-recipe", element: <AddRecipe />}
 ]);
 
 function App() {
--- src/pages/recipes/index.jsx
+++ src/pages/recipes/index.jsx
@@ -15,7 +15,8 @@ export default function Recipes() {
     const searchRecipes = () => {
         setLoading(true);
         //prepare url
-        const url = new URL('https://api.spoonacular.com/recipes/complexSearch');
+        //const url = new URL('https://api.spoonacular.com/recipes/complexSearch');
+        const url = new URL('http://localhost:4000/recipes');
         url.searchParams.append('apiKey', process.env.REACT_APP_SPOONACULAR_API_KEY);
         url.searchParams.append('query', searchItem);//Add the query parameter
 
@@ -23,7 +24,7 @@ export default function Recipes() {
         fetch(url)
             .then((response) => response.json())
             .then((data) => {//update the recipes state
-                setRecipes(data.results);
+                setRecipes(data);
                 //console.log(data);
             })
             .catch((error) => {
@@ -55,7 +56,7 @@ export default function Recipes() {
                     }}>
                         <img src={spinner} width="25%" />
                     </Container>
-                ) : recipes.length > 0 ? recipes.map((recipe) => <RecipeItem key={recipe.id} title={recipe.title} image={recipe.image} id={recipe.id}/>) : (
+                ) : recipes.length > 0 ? recipes.map((recipe) => <RecipeItem key={recipe._id} title={recipe.title} image={recipe.image} id={recipe._id}/>) : (
                     <Container sx={{
                         display: 'flex',
                         justifyContent: 'center'
